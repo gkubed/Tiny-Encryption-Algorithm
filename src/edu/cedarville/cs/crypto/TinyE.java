faf28c94e193aacbde5b15e74c1fee93eb87485d
Tyler Kirtland <tdkirtland@comcast.net>
2015-02-11 22:00:17 -0500

    Completed conversion functions and ECB encryption/decryption

--- src/edu/cedarville/cs/crypto/TinyE.java
+++ src/edu/cedarville/cs/crypto/TinyE.java
@@ -5,11 +5,58 @@ public class TinyE {
     public static enum Mode { ECB, CBC, CTR };
     private final String DELTA = "9E3779B9";
     
+    private Integer[] encryptECB(Integer[] plaintext, Integer[] key, int delta) {
+        Integer[] cipher = new Integer[plaintext.length];
+        
+        for (int p = 0; p < plaintext.length; p += 2) {
+            int sum = 0;
+            cipher[p] = plaintext[p];
+            cipher[p + 1] = plaintext[p + 1];
+            for (int i = 0; i < 32; i++) {
+                sum += delta;
+            
+                cipher[p] = cipher[p] + (((cipher[p + 1] << 4) + key[0]) ^ (cipher[p + 1] + sum) ^ ((cipher[p + 1] >> 5) + key[1]));
+                cipher[p + 1] = cipher[p + 1] + (((cipher[p] << 4) + key[2]) ^ (cipher[p] + sum) ^ ((cipher[p] >> 5) + key[3]));
+            }
+        }
+        
+        return cipher;
+    }
+    
+    private Integer[] decryptECB(Integer[] ciphertext, Integer[] key, int delta) {
+        Integer[] plain = new Integer[ciphertext.length];
+        
+        for (int p = 0; p < ciphertext.length; p += 2) {
+            int sum = delta << 5;
+            
+            plain[p] = ciphertext[p];
+            plain[p + 1] = ciphertext[p + 1];
+            
+            for (int i = 0; i < 32; i++) {
+                plain[p + 1] = plain[p + 1] - (((plain[p] << 4) + key[2]) ^ (plain[p] + sum) ^ ((plain[p] >> 5) + key[3]));
+                plain[p] = plain[p] - (((plain[p + 1] << 4) + key[0]) ^ (plain[p + 1] + sum) ^ ((plain[p + 1] >> 5) + key[1]));
+                sum -= delta;
+            }
+        }
+        
+        return plain;
+    }
+    
     public Integer[] encrypt(Integer[] plaintext, Integer[] key, Mode mode, Integer[] iv) {
+        int delta = Tools.convertFromHexStringToInts(DELTA)[0];
+        if (mode == Mode.ECB) {
+            return encryptECB(plaintext, key, delta);
+        }
+        
         return null;
     }
     
     public Integer[] decrypt(Integer[] ciphertext, Integer[] key, Mode mode, Integer[] iv) {
+        int delta = Tools.convertFromHexStringToInts(DELTA)[0];
+        if (mode == Mode.ECB) {
+            return decryptECB(ciphertext, key, delta);
+        }
+        
         return null;
     }
     
--- src/edu/cedarville/cs/crypto/Tools.java
+++ src/edu/cedarville/cs/crypto/Tools.java
@@ -3,19 +3,52 @@ package edu.cedarville.cs.crypto;
 public class Tools {
     
     public static Integer[] convertFromBytesToInts(byte[] bs) {
-        return null;
+        Integer[] ints = new Integer[bs.length / 4];
+        
+        for (int i = 0; i < ints.length; i++) {
+            ints[i] = 0;
+            for (int j = 0; j < 4; j++) {
+                ints[i] = ints[i] << 8;
+                ints[i] += bs[4 * i + j] & 0xFF;
+            }
+        }
+        
+        return ints;
     }
     
     public static Integer[] convertFromHexStringToInts(String s) {
-        return null;
+        int len = s.length() / 8;
+        Integer[] ints = new Integer[len];
+        
+        for (int i = 0; i < len; i++) {
+            String part = s.substring(8 * i, 8 * i + 8);
+            ints[i] = Integer.parseUnsignedInt(part, 16);            
+        }
+        
+        return ints;
     }
     
     public static byte[] convertFromIntsToBytes(Integer[] ints) {
-        return null;
+        byte[] bytes = new byte[ints.length * 4];
+        
+        for (int i = 0; i < ints.length; i++) {
+            for (int j = 3; j >= 0; j--) {
+                bytes[4 * i + j] = (byte) (ints[i] & 0xFF);
+                ints[i] = ints[i] >> 8;
+            }
+        }
+        
+        return bytes;
     }
     
     public static String convertFromIntsToHexString(Integer[] ints) {
-        return null;
+        String hex = "";
+        
+        for (int i = 0; i < ints.length; i++) {
+            hex += Integer.toHexString(ints[i]).toUpperCase();
+        }
+        
+        return hex;
     }
     
 }
